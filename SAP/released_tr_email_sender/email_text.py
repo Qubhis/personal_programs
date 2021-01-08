31a7196453d648e0a11c8eb02185c448885d3a4f
Qubhis <45180260+Qubhis@users.noreply.github.com>
2021-01-08 10:25:34 +0100

    Create email_text.py

---
+++ SAP/released_tr_email_sender/email_text.py
@@ -0,0 +1,19 @@
+email_body_template = """
+<body>
+    <div>
+       <p>Hello team,<o:p></o:p></p>
+       <p>please move below TR(s) into TST, thank you.<o:p></o:p></p>
+
+           <ol style='margin-top:0cm' start=1 type=1>
+               {}
+           </ol>
+
+        <p></p><p><br></p>
+        <p><b>MY NAME</b></p>
+        <p>MY JOB TITLE<br>
+        COMPANY DIVISION<br>
+        COMPANY NAME<br></p>
+        <p></p>
+    </div>
+</body>
+"""
