8c3037d0723a14b380ee916f4e875040a5ce0957
Qubhis <45180260+Qubhis@users.noreply.github.com>
2021-01-08 10:28:35 +0100

    Create main.py

---
+++ SAP/released_tr_email_sender/main.py
@@ -0,0 +1,14 @@
+import os
+
+from tr_data import TRData
+from ui import TrEmailSender
+
+if __name__ == "__main__":
+    # # change working directory to the main.py directory
+    # os.chdir(os.path.dirname(__file__))
+
+    # initial Transport request data object
+    transport_requests = TRData()
+    # call GUI and pass the instance
+    tr_email_sender_ui = TrEmailSender(transport_requests)
+    
