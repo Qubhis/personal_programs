70ee496a7946eff4104db46df8d7405187644af0
Qubhis <45180260+Qubhis@users.noreply.github.com>
2021-01-09 14:35:15 +0100

    Create main.py

---
+++ SAP/valuation_data_analysis/main.py
@@ -0,0 +1,6 @@
+from functions import fetch_db_data_sapguirpa, clean_and_analyze_data
+
+if __name__ == "__main__":
+    fetch_db_data_sapguirpa()
+    clean_and_analyze_data()
+    print("Analysis done")
